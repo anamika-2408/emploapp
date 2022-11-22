d78dbc7766dad37e6ddbaa170480152ec6840e4a
anamika-2408 <78497091+anamika-2408@users.noreply.github.com>
2022-11-22 21:23:18 +0530

    Add files via upload

---
+++ empmodule.java
@@ -0,0 +1,13 @@
+package com.axis.modal
+
+
+//decalred a class with primary constructor
+//in model class we do what variable declaration
+class Employee (id:Int,name:String,salary:Int) {
+
+    var id:Int=id
+    var name:String=name
+    var salary:Int=salary
+
+
+}
